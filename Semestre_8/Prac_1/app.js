2051b5837af402026f9db95bcb7dc0a412a71e42
Alan Reyes <alanr01rh@gmail.com>
2023-01-25 21:12:29 -0600

    Agrega Rectangulo

--- Semestre_8/Prac_1/app.js
+++ Semestre_8/Prac_1/app.js
@@ -26,3 +26,40 @@ function LimpiarCuadrado() {
     areaCuadrado.value = '';
 }
 
+function CalcularRectangulo() {
+    var largoRectanguloE = document.getElementById('largoRectangulo');        
+    var anchoRectanguloE = document.getElementById('anchoRectangulo');        
+    var largoRectangulo = largoRectanguloE.value;
+    var anchoRectangulo = anchoRectanguloE.value;
+
+    if (largoRectangulo == '' || anchoRectangulo == '') {
+        return false;
+    } else {
+        if (parseInt(largoRectangulo) <= 0 || parseInt(anchoRectangulo) <= 0) {
+            alert('No se permiten números negativos');
+            LimpiarRectangulo();
+        } else {
+            var perimetro = (largoRectangulo * 2) + (anchoRectangulo * 2);        
+            var area = largoRectangulo * anchoRectangulo;
+            var perimetroRectangulo = document.getElementById('perimetroRectangulo');
+            perimetroRectangulo.value = perimetro.toString();
+            var areaRectangulo = document.getElementById('areaRectangulo');
+            areaRectangulo.value = area.toString();
+        }
+    }                
+
+}
+
+function LimpiarRectangulo() {
+    var largoRectangulo = document.getElementById('largoRectangulo');
+    var anchoRectangulo = document.getElementById('anchoRectangulo');
+    var perimetroRectangulo = document.getElementById('perimetroRectangulo');
+    var areaRectangulo = document.getElementById('areaRectangulo');
+    largoRectangulo.value = '';
+    anchoRectangulo.value = '';
+    perimetroRectangulo.value = '';
+    areaRectangulo.value = '';
+}
+
+
+
